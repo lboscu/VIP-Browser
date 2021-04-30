46778f403a0cb6bea76d31daae47d2b223e426c8
lboscu <55314495+lboscu@users.noreply.github.com>
2021-04-30 23:23:51 +0800

    Update mytabwidget.cpp

--- mytabwidget.cpp
+++ mytabwidget.cpp
@@ -2,6 +2,6 @@
 
 MyTabWidget::MyTabWidget(QWidget *parent) : QTabWidget(parent)
 {
-    this->setStyleSheet("QTabBar::tab { width:200px;}");
-    this->setTabsClosable(true);
+    this->setStyleSheet("QTabBar::tab { width:200px;}"); //设置tabBar的宽度固定
+    this->setTabsClosable(true); //设置tabBar上右边的关闭按钮（X）显示出来
 }
